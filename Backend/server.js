079e75684fe477098b010a4792ef0f15946093ee
Hasith manohara <manoharauh.20@uom.lk>
2024-05-19 21:03:21 +0530

    heroku-deploy-changes

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,13 @@ const IndexRoute = require("./Routers/index")
 const connectDatabase = require("./Helpers/database/connectDatabase")
 const customErrorHandler = require("./Middlewares/Errors/customErrorHandler")
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "Frontend", "build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "Frontend", "build", "index.html"));
+    });
+}
+
 dotenv.config({
     path:  './Config/config.env'
 })
