9c883474cd421b95150d114094ec8f9a178dcb15
Hasith manohara <manoharauh.20@uom.lk>
2024-05-20 19:02:54 +0530

    heroku-changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,43 @@
+// const express = require("express")
+// const dotenv = require("dotenv")
+// const cors = require("cors")
+// const path = require("path")
+
+// const IndexRoute = require("./Routers/index")
+// const connectDatabase = require("./Helpers/database/connectDatabase")
+// const customErrorHandler = require("./Middlewares/Errors/customErrorHandler")
+
+// dotenv.config({
+//     path:  './Config/config.env'
+// })
+
+// connectDatabase()
+
+// const app = express() ;
+
+// app.use(express.json())
+// app.use(cors())
+
+// app.use("/",IndexRoute)
+
+// app.use(customErrorHandler)
+
+// const PORT = process.env.PORT || 5000 ;
+
+// app.use(express.static(path.join(__dirname , "public") ))
+
+// const server = app.listen(PORT,()=>{
+
+//     console.log(`Server running on port  ${PORT} : ${process.env.NODE_ENV}`)
+
+// })
+
+// process.on("unhandledRejection",(err , promise) =>{
+//     console.log(`Logged Error : ${err}`)
+
+//     server.close(()=>process.exit(1))
+// })
+
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
@@ -6,13 +46,6 @@ const path = require("path");
 const IndexRoute = require("./Routers/index");
 const connectDatabase = require("./Helpers/database/connectDatabase");
 const customErrorHandler = require("./Middlewares/Errors/customErrorHandler");
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "Frontend", "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "Frontend", "build", "index.html"));
-  });
-}
 
 dotenv.config({
   path: "./Config/config.env",
@@ -25,20 +58,27 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// API routes
 app.use("/", IndexRoute);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
 
 app.use(customErrorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "public")));
-
 const server = app.listen(PORT, () => {
-  console.log(`Server running on port  ${PORT} : ${process.env.NODE_ENV}`);
+  console.log(`Server running on port ${PORT} : ${process.env.NODE_ENV}`);
 });
 
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Logged Error : ${err}`);
-
+  console.log(`Logged Error: ${err}`);
   server.close(() => process.exit(1));
 });
