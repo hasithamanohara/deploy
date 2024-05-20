24d4b271f4043f548975fb06345ffba9166db061
Hasith manohara <manoharauh.20@uom.lk>
2024-05-20 20:49:27 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -59,7 +59,7 @@ app.use(express.json());
 app.use(cors());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../Frontend/build")));
 
 // API routes
 app.use("/", IndexRoute);
@@ -67,7 +67,7 @@ app.use("/", IndexRoute);
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../Frontend/build", "index.html"));
 });
 
 app.use(customErrorHandler);
